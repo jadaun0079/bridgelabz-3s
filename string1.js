341bbe0b02b6c084590e0dc340db406d23ccb91a
jadaun0079 <jatinpratapsingh1000@gmail.com>
2026-01-12 20:29:14 +0530

    thirdQuestion

--- string1.js
+++ string1.js
@@ -14,3 +14,27 @@ function isRotation(s1, s2) {
 // Example
 console.log(isRotation("ABCD", "CDAB")); // true
 
+
+// 3rd question
+function runLengthEncode(str) {
+    if (str.length === 0) return "";
+
+    let result = "";
+    let count = 1;
+
+    for (let i = 1; i <= str.length; i++) {
+        if (str[i] === str[i - 1]) {
+            count++;
+        } else {
+            result += str[i - 1] + count;
+            count = 1;
+        }
+    }
+
+    return result;
+}
+
+// Example
+console.log(runLengthEncode("aaabbc")); // "a3b2c1"
+
+
