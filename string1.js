ad5b3ee7d73e8134605e6a32d9e02ce30365fbb4
jadaun0079 <jatinpratapsingh1000@gmail.com>
2026-01-12 20:23:24 +0530

    secondquestion

--- string1.js
+++ string1.js
@@ -5,3 +5,12 @@ function reverseWords(sentence) {
 console.log(reverseWords("Java is fun")); 
 
 
+// second question 
+function isRotation(s1, s2) {
+    if (s1.length !== s2.length) return false;
+    return (s1 + s1).includes(s2);
+}
+
+// Example
+console.log(isRotation("ABCD", "CDAB")); // true
+
