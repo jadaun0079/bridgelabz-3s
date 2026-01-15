2399b26f4c009df18ccd5f02f6c6c866f5fd26c5
jadaun0079 <jatinpratapsingh1000@gmail.com>
2026-01-15 11:00:42 +0530

    Add index2.js

---
+++ index2.js
@@ -0,0 +1,57 @@
+let s1="Hello";
+let s2='World';
+let s3=`Node.js Backend`;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+//String length
+let text='javascript';
+console.log("length:",text.length);
+
+//Access Character(index)
+let word="Node";
+console.log(word[0]);//N
+console.log(word[1]);//o
+
+//toUpperCase
+let name='sachinn';
+console.log("uppercase:",name.toUpperCase());
+
+//toLowerCAse
+let city="MUMBAI";
+console.log('Lowercase:',city.toLowerCase());
+
+//trim
+let msg=" hello ";
+console.log("Trimed", msg.trim());
+
+//include()
+let sentence="I love JavaScript"
+console.log(sentence.includes("love"));
+
+//startsWith()
+let email="admin@gmail.com";
+console.log("Start with admin",email.startsWith('admin'));
+
+//endsWith()
+console.log("Ennd with .com",email.endsWith(".com"));
+
+//slice()
+let lang="JavaScript";
+console.log("Slice(0,4):",lang.slice(0,4));
+
+//substring()
+console.log("Substring(4,10)",lang.substring(4,10));
+
+//replace()
+
+let greet ="Hello World";
+console.log("Replace:", greet.replace("World","Node"));
+
+//replaceAll()
+let hitext="hi hi hi";
+console.log("Replace All:", hitext.replaceAll("hi","hello"));
+
+//split()
